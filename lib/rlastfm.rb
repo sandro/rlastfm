96a4e297e90e69493322563ba08ed6bff78cd1d2
Sandro Turriate <sandro.turriate@gmail.com>
2009-03-10 00:24:11 -0400

    bumped gem version

--- lib/rlastfm.rb
+++ lib/rlastfm.rb
@@ -1,6 +1,6 @@
 module Rlastfm
   # :stopdoc:
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
 
